7ecd9ee1de3ba5894af26a217fc6a3a2434fd122
Ben <ben@rentenna.com>
2014-04-22 01:06:58 -0400

    [3] match solution

--- 3/3.rkt
+++ 3/3.rkt
@@ -8,4 +8,20 @@
           (first xs)
           (element-at (rest xs) (sub1 i)))))
 
-(provide element-at)
+(define (element-at-match xs i)
+  (match xs
+    ['() empty]
+    [(cons h t) (if (= 0 i) h (element-at-match t (sub1 i)))]))
+
+(define (element-at-fold xs i)
+  ;; 2014-04-22 gah
+  (cadr
+   (foldl (lambda (acc x)
+            (let ([j (car acc)])
+              (if (= i j) (cons (add1 j) x) (cons (add1 j) (cdr acc)))))
+          (cons i 0)
+          xs)))
+
+(provide element-at
+         element-at-match
+         element-at-fold)
