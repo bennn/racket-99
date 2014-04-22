ea7a90242cd2f038bcded0e753255e874b544361
Ben <ben@rentenna.com>
2014-04-22 00:49:16 -0400

    [28] a messy solution, only uses rec because that's all I know

--- 28/28-test.rkt
+++ 28/28-test.rkt
@@ -13,3 +13,11 @@
 
 (check-equal? (lsort (list '() '() (list 1) '())) (list '() '() '() (list 1)))
 (check-equal? (lsort (list (list 1 2 3) (list 5 43) (list 432 111 1) (list 5 543) (list 0 9 8 0) (list 33 333 33) (list 1111))) (list (list 1111) (list 5 43) (list 5 543) (list 1 2 3) (list 432 111 1) (list 33 333 33)(list 0 9 8 0)))
+
+(check-equal? (push-all 1 (list 2 3 4) empty) (list (list 1 4) (list 1 3) (list 1 2)))
+(check-equal? (push-all 1 (list 2 3 4) (list (list 9 9) (list 8 90))) (list (list 1 4) (list 1 3) (list 1 2) (list 9 9) (list 8 90)))
+
+(check-equal? (lfsort (list '() '() (list 1) '())) (list (list 1) '() '() '()))
+(check-equal? (lfsort (list (list 999) (list 1) (list 1) '())) (list '() (list 999) (list 1) (list 1)))
+(check-equal? (lfsort (list (list 1 2 3) (list 4 5) (list 6 7 8) (list 4 5) (list 9 10 11 12) (list 13 14) (list 15)))
+              (list (list 9 10 11 12) (list 15) (list 1 2 3) (list 6 7 8) (list 4 5) (list 4 5) (list 13 14)))
--- 28/28.rkt
+++ 28/28.rkt
@@ -20,7 +20,49 @@
   ; sort lists by length
   (map snd (sort-by-first (map key-by-length xss))))
 
+(define (push-all freq stack out)
+  ;; for each element of stack, add (list freq stack) to out
+  (if (empty? stack) out (push-all freq (cdr stack) (cons (list freq (car stack)) out))))
+  ;; (foldr (lambda (acc x) (cons (list freq x) acc)) out stack))
+
+(define (get-freq-aux freq len stack xs ys)
+  (if (empty? xs) (push-all freq stack ys)
+      (let ([h (car xs)]
+            [t (cdr xs)])
+        (let (
+            [len2 (car h)]
+            [zs (cadr h)])
+        (if (= len2 len)
+            (get-freq-aux (add1 freq) len (cons zs stack) t ys)
+            (get-freq-aux 1 len2 (list zs) t (push-all freq stack ys)))))))
+  
+(define (get-freq xss)
+  (get-freq-aux 0 0 empty xss empty))
+  ;; 2014-04-22 holy fuck, going recursive
+  
+  ;; input = list of (length list) pairs sorted by length, ascending
+  ;; collect the sub-lists of each length, increment frequency throughout
+  ;; push each (freq list) pair when a new length is found
+  ;; (caddr (foldr (lambda (acc p) (let ([freq (car acc)]
+  ;;                         [stack (cadr acc)]
+  ;;                         [out (caddr acc)]
+  ;;                         [freq2 (car p)]
+  ;;                         [xs (cadr p)])
+  ;;                         (if (= freq freq2)
+  ;;                             (list (add1 freq) (cons xs stack) out)
+  ;;                             (list 0 empty (push-all freq stack out)))))
+  ;;        (list 0 empty empty) ;; acc is current-freq, sublist-stack, output
+  ;;        xss)))
+
+(define (lfsort xss)
+  ; sort lists by length frequency
+  ; first sort by length, then get frequency, then sort again
+  (map snd 
+  (sort-by-first (get-freq (sort-by-first (map key-by-length xss))))))
+
 (provide sort-by-first
          snd
          key-by-length
-         lsort)
+         push-all
+         lsort
+         lfsort)
