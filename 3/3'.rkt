21102ca2ed56dafb17d3dbba6e3c62d4515a2230
Ben <ben@rentenna.com>
2014-04-24 17:15:49 -0400

    [3] another solution, typed pattern match on tuple

--- 3/3'.rkt
+++ 3/3'.rkt
@@ -11,3 +11,16 @@
 (assert (equal? 2 (element-at (list 1 2 0 10 21) 1)))
 (assert (equal? #f (element-at '() 30)))
 (assert (equal? #f (element-at '() 0)))
+
+(: element-at-match (All (A) ((Listof A) Integer -> (Option A))))
+(define (element-at-match xs i)
+  (match (cons xs i)
+    [(cons '() n) #f]
+    [(cons (cons h t) n) (if (= 0 n) h (element-at-match t (sub1 i)))]))
+
+(assert (equal? 1 (element-at-match (list 1) 0)))
+(assert (equal? #f (element-at-match (list 1) 3)))
+(assert (equal? 0 (element-at-match (list 1 4 5 0) 3)))
+(assert (equal? 2 (element-at-match (list 1 2 0 10 21) 1)))
+(assert (equal? #f (element-at-match '() 30)))
+(assert (equal? #f (element-at-match '() 0)))
