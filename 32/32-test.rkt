1b57d3b6c3068c09909ec3d66f3b44b26e44fa6e
Ben <ben@rentenna.com>
2014-04-29 18:20:14 -0400

    [32] prettier solution, with cond

--- 32/32-test.rkt
+++ 32/32-test.rkt
@@ -3,4 +3,16 @@
 (require rackunit
          "32.rkt")
 
-(check-equal? (my-gcd 36 63) 9)
+(check-equal? (my-gcd-if 36 63) 9)
+(check-equal? (my-gcd-if 1 1) 1)
+(check-equal? (my-gcd-if 9135 5) 5)
+(check-equal? (my-gcd-if 541 3321) 1)
+(check-equal? (my-gcd-if 18 12) 6)
+(check-equal? (my-gcd-if 99 17) 1)
+
+(check-equal? (my-gcd-cond 36 63) 9)
+(check-equal? (my-gcd-cond 1 1) 1)
+(check-equal? (my-gcd-cond 9135 5) 5)
+(check-equal? (my-gcd-cond 541 3321) 1)
+(check-equal? (my-gcd-cond 18 12) 6)
+(check-equal? (my-gcd-cond 99 17) 1)
--- 32/32.rkt
+++ 32/32.rkt
@@ -1,9 +1,16 @@
 #lang racket
 
-(define (my-gcd n m)
+(define (my-gcd-if n m)
   (if (zero? n) m
   (if (zero? m) n
-  (if (< n m) (my-gcd (- m n) n)
-              (my-gcd (- n m) m)))))
+  (if (< n m) (my-gcd-if (- m n) n)
+              (my-gcd-if (- n m) m)))))
 
-(provide my-gcd)
+(define (my-gcd-cond n m)
+  (cond [(zero? n) m]
+        [(zero? m) n]
+        [(< n m) (my-gcd-cond (- m n) n)]
+        [else (my-gcd-cond (- n m) m)]))
+
+(provide my-gcd-if
+         my-gcd-cond)
