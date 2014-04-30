897694406aa77041345bfb5468a561fcb6ad1ef0
Ben <ben@rentenna.com>
2014-04-29 23:39:52 -0400

    [35] added note: solution's not quite right

--- 35/35.rkt
+++ 35/35.rkt
@@ -3,6 +3,7 @@
 (require "../33/33.rkt"
          "../31/31.rkt")
 
+;; Slightly incorrect: list has no duplicates
 (define (my-prime-factors n)
   (cond [(< n 2) empty]
         [else (stream-fold (lambda (acc x) (if (and (is-prime x) (not (coprime n x))) (cons x acc) acc)) empty (in-range (add1 n) 1 -1))]))
