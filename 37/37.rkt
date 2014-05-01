fc16d894b2de73eff2573aef18ed02db09ba5df0
Ben <ben@rentenna.com>
2014-05-01 02:02:38 -0400

    [37] fixed, problem description had a typo

--- 37/37.rkt
+++ 37/37.rkt
@@ -6,13 +6,9 @@
 (define (my-phi-aux factors acc)
   (match factors
     ['() acc]
-    [(cons (cons p m) t) (my-phi-aux t (+ acc (* (sub1 p) (expt p (sub1 m)))))]))
+    [(cons (cons p m) t) (my-phi-aux t (* acc (* (sub1 p) (expt p (sub1 m)))))]))
 
 (define (my-phi n)
-  (match (my-prime-factors-mult n)
-    ['() (my-totient n)]
-    [xs (my-phi-aux xs 0)]))
-    ;; (foldl (lambda (x acc) (let ([p (first x)][m (second x)]) ((+ acc (* (sub1 p) (expt p (sub1 m)))))))
-    ;;        0 (my-prime-factors-mult n)))
+  (my-phi-aux (my-prime-factors-mult n) 1))
 
 (provide my-phi)
