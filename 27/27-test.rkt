d6fd22e3c16c63ae6315fdd9370f88bd4cb700e4
Ben <ben@rentenna.com>
2014-04-30 23:55:05 -0400

    [27] groups of 2, groups of 3 helpers

--- 27/27-test.rkt
+++ 27/27-test.rkt
@@ -4,9 +4,20 @@
          "27.rkt")
 
 (check-equal? (pairwith 1 '()) '())
-(check-equal? (pairwith 1 (list 1 2 3)) (list (cons 1 3) (cons 1 2) (cons 1 1)))
-(check-equal? (pairwith 9 (list 4 1 1 2 3)) (list (cons 9 3) (cons 9 2) (cons 9 1) (cons 9 1) (cons 9 4)))
+(check-equal? (pairwith 1 (list 1 2 3)) (list (list 1 1) (list 1 2) (list 1 3)))
+(check-equal? (pairwith 9 (list 4 1 1 2 3)) (list (list 9 4) (list 9 1) (list 9 1) (list 9 2) (list 9 3)))
 
 (check-equal? (groupwith 1 '()) '())
-(check-equal? (groupwith 1 (list (list 1) (list 2) (list 3))) (list (list 1 3) (list 1 2) (list 1 1)))
-(check-equal? (groupwith 9 (list (list 4 1) (list 1 2) (list 3))) (list (list 9 3) (list 9 1 2) (list 9 4 1)))
+(check-equal? (groupwith 1 (list (list 1) (list 2) (list 3))) (list (list 1 1) (list 1 2) (list 1 3)))
+(check-equal? (groupwith 9 (list (list 4 1) (list 1 2) (list 3))) (list (list 9 4 1) (list 9 1 2) (list 9 3)))
+
+(check-equal? (groups-of-two empty) empty)
+(check-equal? (groups-of-two (list 1)) empty)
+(check-equal? (groups-of-two (list 19 99)) (list (list 19 99)))
+(check-equal? (groups-of-two (list 1 2 3)) (list (list 1 2) (list 1 3) (list 2 3)))
+
+(check-equal? (groups-of-three empty) empty)
+(check-equal? (groups-of-three (list 1)) empty)
+(check-equal? (groups-of-three (list 19 99)) empty)
+(check-equal? (groups-of-three (list 1 2 3)) (list (list 1 2 3)))
+(check-equal? (groups-of-three (list 1 2 3 4)) (list (list 1 2 3) (list 1 2 4) (list 2 3 4)))
--- 27/27.rkt
+++ 27/27.rkt
@@ -22,11 +22,30 @@
 
 (define (pairwith x xs)
   ;; Create pairs of (x,y) for each y in xs
-  (foldl (lambda (y acc) (cons (cons x y) acc)) empty xs))
+  (reverse (foldl (lambda (y acc) (cons (list x y) acc)) empty xs)))
 
 (define (groupwith x xs)
   ;; Similar to pairwith, but assumes elements of xs are lists themselves
-  (foldl (lambda (ys acc) (cons (append (list x) ys) acc)) empty xs))
+  (reverse (foldl (lambda (ys acc) (cons (append (list x) ys) acc)) empty xs)))
+
+(define (groups-of-two xs)
+  ;; Get all groups of two from a list by pairing each head with the tail
+  (match xs
+    ['() empty]
+    [(list x) empty]
+    [(list x y) (list (list x y))]
+    [(cons h t) (append (pairwith h t) (groups-of-two t))]))
+
+(define (groups-of-three xs)
+  (match xs
+    ['() empty]
+    [(list x) empty]
+    [(list x y) empty]
+    [(list x y z) (list (list x y z))]
+    [(cons h1 (cons h2 t)) (append (groupwith h1 (pairwith h2 t)) (groups-of-three (cons h2 t)))]))
+
+;; (define (group3 xs)
+;;   (let ([of-two 
 
 ;; (define (group-by-size xs size)
 ;;   ; divide list into groups of size 'size'
@@ -40,6 +59,8 @@
 
 (provide pairwith
          groupwith
+         groups-of-two
+         groups-of-three
          ;; group
          ;; group3
          )
