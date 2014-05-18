b2f847cd3679e90e5c0ffc0a5049637475103417
Ben <ben@rentenna.com>
2014-05-18 04:17:15 -0400

    [55] match solution, eliminated a few cases

--- 55/55-test.rkt
+++ 55/55-test.rkt
@@ -11,3 +11,12 @@
 (check-equal? (cbal-tree 4) (list "x" (list "x" empty empty) (list "x" (list "x" empty empty) empty)))
 (check-equal? (cbal-tree 7) (list "x" (list "x" (list "x" empty empty) (list "x" empty empty)) (list "x" (list "x" empty empty) (list "x" empty empty))))
 (check-equal? (cbal-tree 8) (list "x" (list "x" (list "x" empty empty) (list "x" empty empty)) (list "x" (list "x" empty empty) (list "x" (list "x" empty empty) empty))))
+
+(check-equal? (cbal-tree-match -3) #f)
+(check-equal? (cbal-tree-match 0) empty)
+(check-equal? (cbal-tree-match 1) (list "x" empty empty))
+(check-equal? (cbal-tree-match 2) (list "x" empty (list "x" empty empty)))
+(check-equal? (cbal-tree-match 3) (list "x" (list "x" empty empty) (list "x" empty empty)))
+(check-equal? (cbal-tree-match 4) (list "x" (list "x" empty empty) (list "x" (list "x" empty empty) empty)))
+(check-equal? (cbal-tree-match 7) (list "x" (list "x" (list "x" empty empty) (list "x" empty empty)) (list "x" (list "x" empty empty) (list "x" empty empty))))
+(check-equal? (cbal-tree-match 8) (list "x" (list "x" (list "x" empty empty) (list "x" empty empty)) (list "x" (list "x" empty empty) (list "x" (list "x" empty empty) empty))))
--- 55/55.rkt
+++ 55/55.rkt
@@ -8,4 +8,13 @@
         [(= 0 (modulo n 2)) (list "x" (cbal-tree (- (/ n 2) 1)) (cbal-tree (/ n 2)))]
         [else (list "x" (cbal-tree (/ (- n 1) 2)) (cbal-tree (/ (- n 1) 2)))]))
 
-(provide cbal-tree)
+(define (cbal-tree-match n)
+  (match n
+    [0 empty]
+    [1 (list "x" empty empty)]
+    [n #:when (< n 0) #f]
+    [n #:when (= 0 (modulo n 2)) (list "x" (cbal-tree (- (/ n 2) 1)) (cbal-tree (/ n 2)))]
+    [n (list "x" (cbal-tree (/ (- n 1) 2)) (cbal-tree (/ (- n 1) 2)))]))
+
+(provide cbal-tree
+         cbal-tree-match)
