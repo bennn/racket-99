9aba6e3a617f95ea9b711cc652d99be749042731
Ben <ben@rentenna.com>
2014-05-18 04:13:37 -0400

    [55] cond solution

---
+++ 55/55-test.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require rackunit
+         "55.rkt")
+
+(check-equal? (cbal-tree -3) #f)
+(check-equal? (cbal-tree 0) empty)
+(check-equal? (cbal-tree 1) (list "x" empty empty))
+(check-equal? (cbal-tree 2) (list "x" (list "x" empty empty) empty))
+(check-equal? (cbal-tree 3) (list "x" (list "x" empty empty) (list "x" empty empty)))
+(check-equal? (cbal-tree 4) (list "x" (list "x" empty empty) (list "x" (list "x" empty empty) empty)))
+(check-equal? (cbal-tree 7) (list "x" (list "x" (list "x" empty empty) (list "x" empty empty)) (list "x" (list "x" empty empty) (list "x" empty empty))))
+(check-equal? (cbal-tree 8) (list "x" (list "x" (list "x" empty empty) (list "x" empty empty)) (list "x" (list "x" empty empty) (list "x" (list "x" empty empty) empty))))
---
+++ 55/55.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (cbal-tree n)
+  (cond [(< n 0) #f]
+        [(= 0 n) empty]
+        [(= 1 n) (list "x" empty empty)]
+        [(= 2 n) (list "x" (list "x" empty empty) empty)]
+        [(= 0 (modulo n 2)) (list "x" (cbal-tree (- (/ n 2) 1)) (cbal-tree (/ n 2)))]
+        [else (list "x" (cbal-tree (/ (- n 1) 2)) (cbal-tree (/ (- n 1) 2)))]))
+
+(provide cbal-tree)
