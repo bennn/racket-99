4f6678b27589b9cab6369969fd2a2978e5d9f51e
Ben <ben@rentenna.com>
2014-05-23 03:22:01 -0400

    [62b] at level

--- 62/62.rkt
+++ 62/62.rkt
@@ -6,4 +6,11 @@
     [(list _ '() '()) empty]
     [(list x l r) (cons x (append (internals l) (internals r)))]))
 
-(provide internals)
+(define (at-level t n)
+  (match t
+    ['() empty]
+    [(list x '() '()) (if (= 1 n) (list x) empty)]
+    [(list x l r) (if (= 1 n) (list x) (append (at-level l (- n 1)) (at-level r (- n 1))))]))
+
+(provide internals
+         at-level)
