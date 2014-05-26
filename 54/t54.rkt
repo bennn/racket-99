f6c55183ac2949eaa79ddba877374abb64879b1a
Ben <ben@rentenna.com>
2014-05-25 22:41:08 -0400

    [t54] another checkpiont, closer to compiling

--- 54/t54.rkt
+++ 54/t54.rkt
@@ -2,12 +2,16 @@
 
 ;; is tree, typed
 
-(define-type (Tree a) (U Leaf (Node a)))
+(define-type (Tree a) (U Leaf (Node a (Tree a) (Tree a))))
 (struct: Leaf ())
-(struct: (a) Node ([v : a]));; [left: Tree a] [right: Tree a]))
+(struct: (a) Node ([v : a] [left : Tree a] [right : Tree a]))
 
-;; (: is-tree (All (a) (-> (Tree a) (Tree a))))
-;; (define (is-tree t)
-;;   #t)
+(: is-tree (All (A) ((Tree A) -> Boolean)))
+(define (is-tree t)
+  #t)
 
-;; (provide is-tree)
+(provide is-tree)
+
+;; (check-equal? (is-tree (Leaf '())) #t)
+;; (check-equal? (is-tree (Node 1 Leaf Leaf)) #t)
+;; (check-equal? (is-tree (Node "A" Leaf Leaf)) #t)
