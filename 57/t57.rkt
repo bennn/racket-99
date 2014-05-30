82987b50fce81db7745c688a41adff7a3ba0e440
Ben <ben@rentenna.com>
2014-05-29 20:52:48 -0400

    [t57] counting nodes

--- 57/t57.rkt
+++ 57/t57.rkt
@@ -7,15 +7,15 @@
 (struct: Leaf())
 (struct: (A) Node ([val : A] [left : (Tree A)] [right : (Tree A)]))
 (define-type (Tree A) (U Leaf (Node A)))
-(struct: (A) BST ([comparer : (A A -> Boolean)]
+(struct: (A) BST ([compare : (A A -> Boolean)]
                   [tree : (Tree A)]))
 (define-type (BinarySearchTree A) (BST A))
 
 (: insert (All (A) (A (BinarySearchTree A) -> (BinarySearchTree A))))
 (define (insert x bst)
   (match (BST-tree bst)
-    [(Leaf) (BST (BST-comparer bst) (Node x (Leaf) (Leaf)))]
-    [(Node h l r) (let* ([cmp (BST-comparer bst)])
+    [(Leaf) (BST (BST-compare bst) (Node x (Leaf) (Leaf)))]
+    [(Node h l r) (let* ([cmp (BST-compare bst)])
                     (if (cmp x h)
                         (BST cmp (Node h (BST-tree (insert x (BST cmp l))) r))
                         (BST cmp (Node h l (BST-tree (insert x (BST cmp r)))))))]))
@@ -32,4 +32,16 @@
     [(Leaf) (None)]
     [(Node h _ _) (Some h)]))
 
-(assert (match (first-bst (create-bst < (list 1 2 3 4 5 1 2 3))) [(Some x) (= 1 x)] [(None) #f]))
+(: count-nodes (All (A) ((BinarySearchTree A) -> Integer)))
+(define (count-nodes bst)
+  (match (BST-tree bst)
+    [(Leaf) 0]
+    [(Node _ l r) (+ 1
+                     (count-nodes (BST (BST-compare bst) l))
+                     (count-nodes (BST (BST-compare bst) r)))]))
+
+(let ([tree (create-bst < (list 1 2 3 4 5 1 2 3))])
+  (list
+   (assert (match (first-bst tree) [(Some x) (= 1 x)] [(None) #f]))
+   (assert (= 8 (count-nodes tree)))
+   ))
