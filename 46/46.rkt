0227a0778ee1be7e15d2b36cfe47701e200649b8
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-05 13:02:31 -0400

    [46] now parsing strings

--- 46/46.rkt
+++ 46/46.rkt
@@ -7,39 +7,39 @@
     [(cons h _) #:when (equal? h var) #t]
     [(cons _ t) (lookup t var)]))
 
-(define (imp a b)
-  ;; a => b
-  (match (cons a b)
-    [(cons #t #t) #t]
-    [(cons #t #f) #f]
-    [(cons #f #t) #t]
-    [(cons #f #f) #t]))
-
-(define (eq a b)
-  (match (cons a b)
-    [(cons #t #t) #t]
-    [(cons #t #f) #f]
-    [(cons #f #t) #f]
-    [(cons #f #f) #t]))
-
 (define (eval exp env)
   (match exp
-    [(list 'and/2 a b) (and (eval a env) (eval b env))]
-    [(list 'or/2 a b) (or (eval a env) (eval b env))]
+    [(list 'and/2 a b)  (and (eval a env) (eval b env))]
+    [(list 'or/2 a b)   (or (eval a env) (eval b env))]
     [(list 'nand/2 a b) (not (and (eval a env) (eval b env)))]
-    [(list 'nor/2 a b) (not (or (eval a env) (eval b env)))]
-    [(list 'xor/2 a b) (xor (eval a env) (eval b env))]
-    [(list 'imp/2 a b) (imp (eval a env) (eval b env))]
-    [(list 'eq/2 a b) (eq (eval a env) (eval b env))]
-    [var (lookup env var)]))
+    [(list 'nor/2 a b)  (not (or (eval a env) (eval b env)))]
+    [(list 'xor/2 a b)  (xor (eval a env) (eval b env))]
+    [(list 'imp/2 a b)  (or (not (eval a env)) (and (eval a env) (eval b env)))]
+    [(list 'eq/2 a b)   (eq? (eval a env) (eval b env))]
+    [var                (lookup env var)]))
 
-(define (table/2 exp varA varB)
+(define (parse-until-eof inp)
+  (match (read inp)
+    [x #:when (eof-object? x) empty]
+    [x                        (cons x (parse-until-eof inp))]))
+
+(define (parse-str str)
+  ;; read whole string into a list
+  (parse-until-eof (open-input-string str)))
+
+(define (table/2 varA varB str-exp)
   ;; evaluate [exp] with every combination of truth values for [varA] and [varB]
-  (printf " ~a  ~a |  R\n" varA varB)
-  (printf "----------\n")
-  (for-each (lambda (env)
-              (printf "~a ~a | ~a\n" (lookup env varA) (lookup env varB) (eval exp env))
-              ) (list (list) (list varA) (list varB) (list varA varB))))
+  ;; [exp] is a string like "and A B"
+  (let ([exp (parse-str str-exp)])
+    (begin
+     (printf "Results for expression: ~a\n" str-exp)
+     (printf " ~a  ~a |  R\n" varA varB)
+     (printf "----------\n")
+     (printf "#t #t | ~a\n" (eval exp (list varA varB)))
+     (printf "#t #f | ~a\n" (eval exp (list varA)))
+     (printf "#f #t | ~a\n" (eval exp (list varB)))
+     (printf "#f #f | ~a\n\n" (eval exp empty))
+    )))
 
 (provide eval
          table/2)
