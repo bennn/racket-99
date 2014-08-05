2594dbff18ed01f14831ab03aa8c3de383ad006d
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-05 01:19:01 -0400

    [1] prettier impl

--- 1/1.rkt
+++ 1/1.rkt
@@ -1,14 +1,20 @@
 #lang racket
 ;; Find the last box in a list
 
-(define (my-last-acc prev xs)
-  (if (empty? xs)
-      prev
-      (my-last-acc (first xs) (rest xs))))
+;; (define (my-last-acc prev xs)
+;;   (if (empty? xs)
+;;       prev
+;;       (my-last-acc (first xs) (rest xs))))
 
+;; (define (my-last xs)
+;;   (if (empty? xs)
+;;       #f
+;;       (my-last-acc (first xs) (rest xs))))
+
+;; 2014-08-05: prettier
 (define (my-last xs)
-  (if (empty? xs)
-      #f
-      (my-last-acc (first xs) (rest xs))))
+  (cond [(empty? xs)       #f]
+        [(empty? (cdr xs)) (car xs)]
+        [else              (my-last (cdr xs))]))
   
 (provide my-last)
