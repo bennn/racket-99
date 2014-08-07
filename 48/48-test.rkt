789be9e3d8e30c954ab55a8c47ad75a6aa387372
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-07 03:09:35 -0400

    [48] a few more tests

--- 48/48-test.rkt
+++ 48/48-test.rkt
@@ -6,13 +6,9 @@
 (table/2 (list 'A 'B) "A eq B")
 (table/2 (list 'A 'B 'C 'D 'E) "A eq B")
 
-;; (table/2 'A 'B "A imp B")
+(table/2 (list 'A 'B) "(A imp B)")
 
-;; (table/2 'A 'B "(A imp B)")
+(table/2 (list 'A 'B 'C) "D imp C")
 
-;; (table/2 'A 'B "D imp C")
-
-;; (table/2 'A 'B "D imp C")
-
-;; (table/2 'A 'B "A and (A or (not B))")
+(table/2 (list 'A 'D) "A and (A or (not B))")
 
