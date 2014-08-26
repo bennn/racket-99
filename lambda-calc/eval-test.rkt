467bd44ab673885cb92a2200e56d86b9662097f5
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-26 05:52:08 -0400

    [lambda] thunking omega

--- lambda-calc/eval-test.rkt
+++ lambda-calc/eval-test.rkt
@@ -14,3 +14,8 @@
 (check-equal? (eval (list 'force (list 'delay 42)) '()) 42)
 
 (check-equal? (eval (list 'delay (list 'f 'f 'f)) '()) (cons 'thunk (list 'f 'f 'f)))
+
+(check-equal? (eval (list (list 'fun 'x 42)
+                          (list 'delay (list (list 'fun 'x (list 'x 'x))
+                                       (list 'fun 'x (list 'x 'x))))) '()) 42)
+
