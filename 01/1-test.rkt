50b3aad81494a933bc104ddf7c4fcd1594e94018
Ben Greenman <greenman.b@husky.neu.edu>
2014-09-21 15:24:48 -0400

    [01] remodeled to rackunit-chk. Much nicer

--- 01/1-test.rkt
+++ 01/1-test.rkt
@@ -1,13 +1,24 @@
 #lang racket
 
-(require rackunit
+(require
+         rackunit-chk/rackunit/chk
          "1.rkt")
 
-(check-equal? #f (my-last empty))
-(check-equal? 1 (my-last (list 1)))
-(check-equal? 1 (my-last (list 2 1)))
-(check-equal? 4 (my-last (list 2 1 3 2 1 0 4)))
-(check-equal? 99 (my-last (cons (list 1 1 1) (list 2 1 99))))
+(chk
+  [#:f (my-last empty)]
+  (my-last (list 1)) 1
+  (my-last (list 2 1)) 1
+  (my-last (list 2 1 3 2 1 0 4)) 4
+  (my-last (cons (list 1 1 1) (list 2 1 99))) 99
+  (my-last '(2 1)) 1
+  #:f (my-last '())
+)
 
-(check-equal? 1 (my-last '(2 1)))
-(check-equal? #f (my-last '()))
+; (check-equal? #f (my-last empty))
+; (check-equal? 1 (my-last (list 1)))
+; (check-equal? 1 (my-last (list 2 1)))
+; (check-equal? 4 (my-last (list 2 1 3 2 1 0 4)))
+; (check-equal? 99 (my-last (cons (list 1 1 1) (list 2 1 99))))
+; 
+; (check-equal? 1 (my-last '(2 1)))
+; (check-equal? #f (my-last '()))
